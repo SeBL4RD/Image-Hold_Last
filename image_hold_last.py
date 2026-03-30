ea6bc20d34fdcf6d61711deb2b50ed19a45365f1
SeBL4RD <45276149+SeBL4RD@users.noreply.github.com>
2026-03-30 22:23:04 +0200

    Refactor hold method to use instance variable for last image instead of class-level cache

--- image_hold_last.py
+++ image_hold_last.py
@@ -2,7 +2,6 @@ import torch
 import time
 
 class ImageHoldLast:
-    _cache = {}
 
     @classmethod
     def INPUT_TYPES(cls):
@@ -21,9 +20,9 @@ class ImageHoldLast:
 
     def hold(self, image=None):
         if image is not None:
-            ImageHoldLast._cache["last"] = image
-        if "last" in ImageHoldLast._cache:
-            return (ImageHoldLast._cache["last"],)
+            self._last_image = image
+        if hasattr(self, "_last_image"):
+            return (self._last_image,)
         return (torch.zeros(1, 1, 1, 3),)
 
 NODE_CLASS_MAPPINGS = {"ImageHoldLast": ImageHoldLast}
